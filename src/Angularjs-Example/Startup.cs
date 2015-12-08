02a26bdb651c0a9fea8a8c868f09e405e10a3752
Ken Lovely <kenl_mcse@comcast.net>
2015-12-07 21:54:51 -0500

    Running Demo

---
+++ src/Angularjs-Example/Startup.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNet.Builder;
+using Microsoft.AspNet.Hosting;
+using Microsoft.AspNet.Http;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.PlatformAbstractions;
+
+namespace Angularjs_Example
+{
+    public class Startup
+    {
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit http://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder application, IApplicationEnvironment environment)
+        {
+            application.UseIISPlatformHandler();
+            
+            application.UseStaticFiles();
+            application.Run(async (context) =>
+            {
+                var indexPage = System.IO.File.ReadAllText($"{environment.ApplicationBasePath}/index.html");
+                await context.Response.WriteAsync(indexPage);
+            });
+        }
+
+        // Entry point for the application.
+        public static void Main(string[] args) => WebApplication.Run<Startup>(args);
+    }
+}
---
+++ src/Angularjs-Example/wwwroot/js/Site.js
@@ -0,0 +1,10 @@
+﻿angular.module('myApp', ['ngSanitize'])
+.controller('myController', function ($scope, $timeout) {
+    
+    $scope.view = "splash";
+    $timeout(function () { $scope.view = "page1"; }, 3000);
+    
+    $scope.setView = function (view) {
+        $scope.view = view;
+    }
+})
